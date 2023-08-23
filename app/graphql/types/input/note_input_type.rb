c4a157451a936b9c420a7a1738848215437aec32
sachinkreeti <sgupta@kreeti.com>
2023-08-24 04:06:15 +0530

    show an example of how to add description

--- app/graphql/types/input/note_input_type.rb
+++ app/graphql/types/input/note_input_type.rb
@@ -1,7 +1,7 @@
 module Types
   module Input
     class NoteInputType < Types::BaseInputObject
-      argument :title, String, required: true
+      argument :title, String, required: true, description: "title of note"
       argument :body, String, required: true
     end
   end
